d2c90e2108ae394abf726d35e5b1b1d1e67dfc1f
sakurataiko1 <49112677+sakurataiko1@users.noreply.github.com>
2022-09-19 15:26:22 +0900

    Add files via upload

---
+++ pySide6_slider_decimal/Ui_MainWindow.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'form.ui'
+##
+## Created by: Qt User Interface Compiler version 6.3.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QHBoxLayout, QLabel, QLineEdit,
+    QMainWindow, QMenuBar, QSizePolicy, QSlider,
+    QSpacerItem, QStatusBar, QVBoxLayout, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(438, 140)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.verticalLayout = QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.label1 = QLabel(self.centralwidget)
+        self.label1.setObjectName(u"label1")
+
+        self.horizontalLayout.addWidget(self.label1)
+
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout.addItem(self.horizontalSpacer)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.lineEdit1 = QLineEdit(self.centralwidget)
+        self.lineEdit1.setObjectName(u"lineEdit1")
+        self.lineEdit1.setMinimumSize(QSize(0, 0))
+        self.lineEdit1.setMaximumSize(QSize(100, 16777215))
+
+        self.horizontalLayout_2.addWidget(self.lineEdit1)
+
+        self.slider1 = QSlider(self.centralwidget)
+        self.slider1.setObjectName(u"slider1")
+        self.slider1.setOrientation(Qt.Horizontal)
+
+        self.horizontalLayout_2.addWidget(self.slider1)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 438, 26))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.label1.setText(QCoreApplication.translate("MainWindow", u"\u5c0f\u6570\u4ee5\u4e0b\u5bfe\u5fdc\u30b9\u30e9\u30a4\u30c0\u30fc", None))
+    # retranslateUi
+
--- pySide6_slider_decimal/mainwindow.py
+++ pySide6_slider_decimal/mainwindow.py
@@ -1,1 +1,50 @@
-test
+# This Python file uses the following encoding: utf-8
+import os
+from pathlib import Path
+import sys
+
+from PySide6 import *
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+from PySide6.QtWidgets import *
+
+from Ui_MainWindow import Ui_MainWindow
+
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setupUi(self)
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        # スライダー　小数点対応(QSliderは整数しか対応していない。小数にも対応できるようにする)
+        self.ui.lineEdit1.editingFinished.connect(self.func_on_lineEdit1_editingFinished)
+        self.ui.slider1.valueChanged.connect(self.func_on_slider1_valueChanged)
+
+        #　GUIフォーム初期値
+        self.ui.slider1.setMinimum(1)
+        self.ui.slider1.setMaximum(1000)
+
+    def func_on_slider1_valueChanged(self):
+        # スライダーは0以下数値を扱えないので、小数点以下にも対応できるように換算
+        keta = -1 * 3
+        value = self.ui.slider1.value()
+        real_value = value * (pow(10, keta)) #round(value * (pow(10, -3)), 3)
+        str_value = "%0.*f" % (abs(keta), real_value)
+        # テキストボックスに値を入れる (lineEdit にしているので valueChangedにループはしない)
+        self.ui.lineEdit1.setText(str_value)
+        #　print("[DEBUG]01 MainWindow.py-func_on_slider1_valueChanged value=", value, " real_value=", real_value, " textBox=" , self.ui.lineEdit1.text())
+
+    def func_on_lineEdit1_editingFinished(self):
+        #print("[DEBUG]01 MainWindow.py-func_on_model1_editingFinished\n")
+        keta = 3
+        value = float(self.ui.lineEdit1.text())
+        self.ui.slider1.setValue(int(value * 10 ** keta)) # スライダーは0以下数値を扱えないので、小数点以下にも対応できるように換算
+
+if __name__ == "__main__":
+    app = QApplication([])
+    widget = MainWindow()
+    widget.show()
+    sys.exit(app.exec_())
